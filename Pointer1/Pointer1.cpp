95e14fc91a2aabd976c16f4d75b121b3821743b6
ibnatysalsabila <salsajpr01@gmail.com>
2024-05-08 14:03:44 +0700

    Add project files.

---
+++ Pointer1/Pointer1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+public:
+    int nim;
+    void showNim() {
+        cout << "No Induk = " << nim << endl;
+    }
+};
+
+int main() {
+
+    mahasiswa mhs{ 1 }; // object mhs
+    mhs.showNim(); // member acces operator
+
+    mahasiswa& refMhs = mhs; // pointer reference refmhs
+    refMhs.nim = 2; // member access operator
+    mhs.showNim();
+
+    mahasiswa* pMhs = &mhs;
+    pMhs->nim = 3;
+    pMhs->showNim();
+    return 0;
+
+}
